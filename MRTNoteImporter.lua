58d75658740dc803cf23a288a83fb10c2c790409
David Groves <git@fibrecat.org>
2023-02-08 01:04:31 +0000

    Added base64 encoding / decoding

--- MRTNoteImporter.lua
+++ MRTNoteImporter.lua
@@ -1,6 +1,7 @@
 local MRTNI = LibStub("AceAddon-3.0"):NewAddon("MRTNI", "AceConsole-3.0")
 local AceGUI = LibStub("AceGUI-3.0")
 local JSON = LibStub("LibJSON-1.0")
+local LibBase64 = LibStub('LibBase64-1.0')
 
 local frameShown = false
 local textStore = ""
@@ -32,7 +33,7 @@ local function showFrame()
     frame:SetLayout("List")
 
     local editbox = AceGUI:Create("MultiLineEditBox")
-    editbox:SetLabel("Insert JSON here:")
+    editbox:SetLabel("Insert JSON / Base64 here:")
     editbox:DisableButton(true)
     editbox:SetFocus(true)
     editbox:SetFullWidth(true)
@@ -42,16 +43,21 @@ local function showFrame()
     editbox:SetCallback("OnTextChanged", function(widget, event, text) textStore = text end)
     frame:AddChild(editbox)
 
+    local base64 = AceGUI:Create("CheckBox")
+    base64:SetLabel("Base64")
+    base64:SetValue(true)
+    frame:AddChild(base64)
+
     local setbutton = AceGUI:Create("Button")
     setbutton:SetText("Set Notes")
     setbutton:SetWidth(100)
-    setbutton:SetCallback("OnClick", function() MRTNI:ConvertInputToMRTNotes(textStore, frame); end)
+    setbutton:SetCallback("OnClick", function() MRTNI:ConvertInputToMRTNotes(textStore, frame, base64:GetValue()); end)
     frame:AddChild(setbutton)
 
     local fetchbutton = AceGUI:Create("Button")
     fetchbutton:SetText("Fetch Notes")
     fetchbutton:SetWidth(100)
-    fetchbutton:SetCallback("OnClick", function() MRTNI:ConvertMRTNotesToInput(editbox); end)
+    fetchbutton:SetCallback("OnClick", function() MRTNI:ConvertMRTNotesToInput(editbox, base64:GetValue()); end)
     frame:AddChild(fetchbutton)
     end
 
@@ -59,14 +65,25 @@ function MRTNI:ChatCommand()
     showFrame()
 end
 
-function MRTNI:ConvertInputToMRTNotes(t, frame)
+function MRTNI:ConvertInputToMRTNotes(t, frame, base64)
+
     -- Delete the existing notes
     VMRT.Note.Black = {}
     VMRT.Note.BlackNames = {}
 
     -- Make the new notes
     local i = 1
-    local status, parsedTable = pcall(JSON.Deserialize, t)
+    local status = false
+    local parsedTable = ""
+    
+    if base64 then
+        local decoded_base64 = ""
+        decoded_base64 = LibBase64:Decode(t)
+        status, parsedTable = pcall(JSON.Deserialize, decoded_base64)
+    else
+        status, parsedTable = pcall(JSON.Deserialize, t)
+    end
+
     if not status then
         frame:SetStatusText(MRTNI:strsplit(parsedTable, ":")[3] .. MRTNI:strsplit(parsedTable, ":")[4])
     else
@@ -79,7 +96,7 @@ function MRTNI:ConvertInputToMRTNotes(t, frame)
     end
 end
 
-function MRTNI:ConvertMRTNotesToInput(editbox)
+function MRTNI:ConvertMRTNotesToInput(editbox, base64)
     if #VMRT.Note.BlackNames == 0 then
         editbox:SetText("Cannot read notes. Please update notes in /mrt, or paste JSON here to set mrt notes.")
         return
@@ -91,5 +108,9 @@ function MRTNI:ConvertMRTNotesToInput(editbox)
         t[VMRT.Note.BlackNames[i]] = VMRT.Note.Black[i]
     end
     textStore = JSON.Serialize(t)
+
+    if base64 then
+        textStore = LibBase64:Encode(textStore)
+    end
     editbox:SetText(textStore)
 end
